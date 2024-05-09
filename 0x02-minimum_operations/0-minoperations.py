9bd294cd6c4d519669b14c0be72d286618c0df9f
kaluogba <kaluogba1990@gmail.com>
2024-05-09 06:28:03 -0700

    minimum ops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """fewest number of operations needed to result in exactly n H characters.
+
+    Args:
+        n (int): The target number of H characters.
+
+    Returns:
+        int: The minimum number of operations required.
+
+    """
+    if n <= 1:
+        return 0
+
+    divisor = 2
+    min_ops = 0
+
+    # Perform prime factorization to calculate minimum operations
+    while n > 1:
+        if n % divisor == 0:
+            n //= divisor
+            min_ops += divisor
+        else:
+            divisor += 1
+
+    return min_ops
+
+
+if __name__ == "__main__":
+    # Test cases
+    n = 4
+    print("Min # of operations to reach {} char: {}".format(
+        n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} char: {}".format(
+        n, minOperations(n)))
